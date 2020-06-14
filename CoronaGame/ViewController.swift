dde9e7695fb9aa7b1367dc66903902033f14c213
Kittyjosh <joshua.shi@gmail.com>
2020-06-13 22:56:08 -0700

    Initial Commit

---
+++ CoronaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaGame
+//
+//  Created by Joshua Shi on 6/13/20.
+//  Copyright © 2020 Joshua Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoronaGameTests/CoronaGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoronaGameTests.swift
+//  CoronaGameTests
+//
+//  Created by Joshua Shi on 6/13/20.
+//  Copyright © 2020 Joshua Shi. All rights reserved.
+//
+
+import XCTest
+@testable import CoronaGame
+
+class CoronaGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
